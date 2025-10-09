03ce6234516e4e8700807aafeb74f0108cc2ed4b
Zahid Buğra Sarıcan <115879495+zbraven@users.noreply.github.com>
2025-10-09 13:34:31 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Crystalwaste
+
+Developed with Unreal Engine 5
---
+++ Source/Crystalwaste/Private/Player/CPlayerCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/CPlayerCharacter.h"
+
+// Sets default values
+ACPlayerCharacter::ACPlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
